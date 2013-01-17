fa0b33eecca21389f032675d7c06755e2679a399
Franchesco Romero <elchesco@gmail.com>
2013-01-17 11:52:11 -0600

    modified:   vBulletinPlugins/Site Registration/product-site_registration.xml 	modified:   vBulletinPlugins/Site Registration/upload/includes/site-registration/js/common.js 	modified:   vb/includes/site-registration/css/all-ie-only.css 	modified:   vb/includes/site-registration/js/common.js

--- vBulletinPlugins/Site Registration/upload/includes/site-registration/js/common.js
+++ vBulletinPlugins/Site Registration/upload/includes/site-registration/js/common.js
@@ -42,6 +42,82 @@ closeTnC = function() {
 }
 
 
+/*!
+ * jQuery Placeholder v0.9
+ * http://webcloud.se/code/jQuery-placeholder/
+ *
+ * Copyright 2011, Daniel Stocks
+ * Released under the MIT, BSD, and GPL Licenses.
+ *
+ */
+(function($, window, undefined ) {
+
+  window.Placeholder = function(origin) {
+
+    var
+      self = this,
+      o = origin.get(0),
+      decoy = $('<'+ o.tagName +'>', {
+      val : origin.attr("placeholder"),
+      click : function(e) {
+        e.preventDefault();
+        self.off();
+        origin.focus();
+      }
+    }).addClass("placeholder");
+
+    self.decoy = decoy;
+    self.origin = origin;
+
+    origin
+      .after(decoy)
+      .change($.proxy(self.on, self))
+      .blur($.proxy(self.on, self));
+
+    // Turn on the light
+    self.on();
+
+    // Copy origin styles
+    var styles = (function() {
+      if(window.getComputedStyle) {
+        return window.getComputedStyle(o, null)
+      }
+      if(o.currentStyle) {
+        return o.currentStyle;
+      }
+    })();
+
+    // Apply styles to decoy
+    $.each(styles, function(k, prop) {
+      if(o.currentStyle) {
+        decoy.css(k, prop);
+      }
+      if(window.getComputedStyle) {
+        decoy.css(prop, styles.getPropertyValue(prop));
+      }
+    });
+  }
+
+  Placeholder.prototype = {
+
+    // Turn the placeholder on
+    on : function() {
+      if(this.origin.val())
+        return this.off();
+      this.decoy.show();
+      this.origin.hide();
+    },
+
+    // turn the placeholder off
+    off : function() {
+      this.decoy.hide();
+      this.origin.show();
+    }
+  }
+})(jQuery, this);
+
+
+
  
  
 
@@ -49,6 +125,8 @@ closeTnC = function() {
 * lazy load and bootstrap all elements
 */
 jQuery(document).ready(function(jQuery) {
+
+    jQuery('input[placeholder], textarea[placeholder]').placeholder();
 
     //initialize any facebox
     if (jQuery('a[rel*=facebox]').exists()) {
