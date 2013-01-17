87ad74e3bf162268e1deb15a0326de2332074e58
Franchesco Romero <elchesco@gmail.com>
2013-01-17 16:49:29 -0600

    modified:   vBulletinPlugins/Site Registration/upload/includes/site-registration/php/index.php 	modified:   vb/includes/site-registration/php/index.php

--- vBulletinPlugins/Site Registration/upload/includes/site-registration/php/index.php
+++ vBulletinPlugins/Site Registration/upload/includes/site-registration/php/index.php
@@ -207,7 +207,7 @@ case 'complete_your_profile':
         }
     } else {
         //use default image
-        $default_image = "../../"."images/misc/unknown.gif";
+        $default_image = "../../../"."images/misc/unknown.gif";
   
         
         list($width, $height, $type, $attr) = getimagesize($default_image);
