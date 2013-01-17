ca5ced94c5547b486ad2353978e6790f45857fb1
Franchesco Romero <elchesco@gmail.com>
2013-01-17 16:35:12 -0600

    modified:   vBulletinPlugins/Site Registration/upload/includes/site-registration/php/index.php

--- vBulletinPlugins/Site Registration/upload/includes/site-registration/php/index.php
+++ vBulletinPlugins/Site Registration/upload/includes/site-registration/php/index.php
@@ -208,6 +208,9 @@ case 'complete_your_profile':
     } else {
         //use default image
         $default_image = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "images/misc/unknown.gif";
+        
+        
+        
         list($width, $height, $type, $attr) = getimagesize($default_image);
 
         $userid = $_SESSION['site_registration']['userid'];
@@ -322,7 +325,7 @@ case 'complete_your_profile':
     }
 
     $arr = array("valid_entries" => $valid_entries, "messages" => $messages,
-            "rows" => $rows);
+            "rows" => $rows, "image"=> $default_image);
 
     json_headers($arr);
 
