80d120a8f7a6ba43cfdc378edf0963c60b513162
Carlos Santana <carlos@milkzoft.com>
2013-02-08 12:53:39 -0600

    VBNexus

--- vb/includes/site-registration/php/index.php
+++ vb/includes/site-registration/php/index.php
@@ -14,12 +14,11 @@ error_reporting(E_ALL & ~E_NOTICE & ~8192);
 define('CSRF_PROTECTION', true);
 define('THIS_SCRIPT', 'site-registration');
 
-die(var_dump(DIR . '/includes/vbnexus4.1.5/vBNexus.php'));
 require_once("site_registration_functions.php");
 require_once("rfc822.php");
 
 if($show['vbnexus_button_fb']) {    
-    require_once(DIR . '/includes/vbnexus4.1.5/vBNexus.php');
+    require_once('vbnexus4.1.5/vBNexus.php');
 }
 
 //include required files
@@ -28,10 +27,6 @@ require_once('global.php');
 require_once('functions_user.php');
 require_once('functions_misc.php');
 require_once('functions_login.php');
-
-if($show['vbnexus_button_fb']) {
-    require_once('');
-}
 
 if (!session_id()) {
     session_start();
