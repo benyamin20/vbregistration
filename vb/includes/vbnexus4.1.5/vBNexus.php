cb02770e9725d84d3a3375c3774fe52e2dd86a08
Carlos Santana <carlos@milkzoft.com>
2013-02-11 16:46:56 -0600

    vbnexus

--- vb/includes/vbnexus4.1.5/vBNexus.php
+++ vb/includes/vbnexus4.1.5/vBNexus.php
@@ -125,7 +125,7 @@ class vBNexus {
      * Whether a service (FB Connect | Google Friend Connect) is enabled
      */
     public function isEnabled($svc) {
-        die(var_dump($this->enabled));
+        die(var_dump($svc));
         return isset($this->enabled[$svc]) ? TRUE : FALSE;
     }
 
