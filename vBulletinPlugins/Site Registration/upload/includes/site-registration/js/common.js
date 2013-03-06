f4387fa89bb91959152aba310bc66729187433f9
Franchesco Romero <elchesco@gmail.com>
2013-03-06 16:20:14 -0600

    modified:   Site Registration/upload/includes/site-registration/js/common.js 	modified:   ../vb/includes/site-registration/js/common.js

--- vBulletinPlugins/Site Registration/upload/includes/site-registration/js/common.js
+++ vBulletinPlugins/Site Registration/upload/includes/site-registration/js/common.js
@@ -323,7 +323,7 @@ jQuery(document).ready(function (jQuery) {
                         changeMonth: true,
                         constrainInput: true,
                         hideIfNoPrevNext: false,
-                        yearRange: '1900:2100',
+                        yearRange: '1902:2100',
                         onSelect: function (date) {
                             if (jQuery("#placeholder-datepicker").exists()) {
                                 jQuery("#placeholder-datepicker").remove();
